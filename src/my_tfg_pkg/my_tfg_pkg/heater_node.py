686f906d46ef4f3fe1c13779163bfac2b4e06b79
Pedro Parrilla <pedroparrilla@protonmail.com>
2021-10-18 21:16:16 +0200

    Update heater node

--- src/my_tfg_pkg/my_tfg_pkg/heater_node.py
+++ src/my_tfg_pkg/my_tfg_pkg/heater_node.py
@@ -2,24 +2,41 @@
 import rclpy
 from rclpy.node import Node
 
-from my_tfg_interfaces.srv import SensorArea
+from custom_node_message.msg import FloatDataNode, StatusNode
+
 
 class HeaterNode(Node):
     def __init__(self):
-        super().__init__("heater_node")
+        super().__init__("heater_32")
         self.declare_parameter("pos_x", 0.0)
         self.declare_parameter("pos_y", 0.0)
-        self.pos_x = self.get_parameter("pos_x").value
-        self.pos_y = self.get_parameter("pos_y").value
-        self.server_ = self.create_service(SensorArea, "sensor_area", self.callback_heater)
-        self.get_logger().info("Heater server has been started.")
 
-    def callback_heater(self, request, response):
-        # Implement busy
-        self.pos_x = request.pos_x
-        self.pos_y = request.pos_y
-        response.success = True
-        return response
+        self.status_node_ = StatusNode()
+        self.status_node_.device_id = 32
+        self.status_node_.work_status = 0
+        self.status_node_.position.x = self.get_parameter("pos_x").value
+        self.status_node_.position.y = self.get_parameter("pos_y").value
+
+        self.temperature_to_act_ = 12.0
+
+        self.temperature_subscriber_ = self.create_subscription(
+            FloatDataNode, "temperature", self.callback_sensor_data, 10)
+        self.get_logger().info("Heater_" + str(self.status_node_.device_id) + " has been started.")
+
+    def callback_sensor_data(self, msg):
+        self.get_logger().info("Device id: " + str(msg.device_id) + " Temperature: " + str(msg.data))
+        if abs(msg.position.x - self.status_node_.position.x) < 3.0 and abs(msg.position.y - self.status_node_.position.y) < 3.0:
+            if msg.data >= self.temperature_to_act_ and self.status_node_.work_status == 0:
+                self.status_node_.work_status = 1
+                self.get_logger().info("heater_" + str(self.status_node_.device_id) + " is on")
+            elif msg.data < self.temperature_to_act_ and self.status_node_.work_status != 0:
+                self.status_node_.work_status = 0
+                self.get_logger().info("heater_" + str(self.status_node_.device_id) + " is off")
+
+    def publish_status(self):
+        msg = self.sensor_data_
+        self.sensor_publisher_.publish(msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
